ccdea9f0ad18d736591581f30beba02ea5fac0bb
tylerbert31 <tbaring_ccs@uspf.edu.ph>
2026-02-07 00:34:55 +0800

    Initial

---
+++ src/lib/query.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+class query {
+  private get = axios.get;
+  private rootUrl: string = "";
+
+  constructor() {}
+}
+
+const Query = new query();
+export default Query;
